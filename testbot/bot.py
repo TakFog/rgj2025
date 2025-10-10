705962b6079aa4d0e98c04234be3caff0a22e38f
takfog <takfog@gmail.com>
2025-10-11 01:13:09 +0200

    with gemini

--- testbot/bot.py
+++ testbot/bot.py
@@ -4,8 +4,12 @@
 import discord
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv('.env')
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+aiclient = genai.Client()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,5 +27,11 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$llm'):
+        response = aiclient.models.generate_content(
+            model="gemini-2.0-flash-lite", contents=message.content[4:]
+        )
+        await message.channel.send(response.text[:1990]) # limit 2000 chars
 
 client.run(os.environ['DISCORD_TOKEN'])
