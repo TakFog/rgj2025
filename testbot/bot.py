3522ffb38e5aab0188dede919249402704fc07c0
takfog <takfog@gmail.com>
2025-10-11 10:28:03 +0200

    chat history

--- testbot/bot.py
+++ testbot/bot.py
@@ -7,10 +7,21 @@ from dotenv import load_dotenv
 from google import genai
 import asyncio
 
+class Llm:
+    def __init__(self, model: str):
+        self.model = model
+        # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+        self.aiclient = genai.Client()
+
+    def generate_content(self, input):
+        response = self.aiclient.models.generate_content(
+            model=self.model, contents=input
+        )
+        return response.text
+
 load_dotenv('.env')
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-aiclient = genai.Client()
+llm = Llm("gemini-2.0-flash-lite")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,7 +31,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await asyncio.sleep(10)        # delay
+    # await asyncio.sleep(10)        # delay
     await send_message(os.environ.get('CHANNEL_ID'), "Sono pronto!")
 
 @client.event
@@ -32,10 +43,8 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
     if message.content.startswith('$llm'):
-        response = aiclient.models.generate_content(
-            model="gemini-2.0-flash-lite", contents=message.content[4:]
-        )
-        await message.channel.send(response.text[:1990]) # limit 2000 chars
+        response = llm.generate_content(message.content[4:])
+        await message.channel.send(response[:1990]) # limit 2000 chars
 
     if message.content.startswith('$img'):
         file = discord.File("pergamena_codex.jpg", filename="pergamena.jpg")
@@ -47,6 +56,10 @@ async def send_message(channel_id, message):
     if channel_id is None:
         return
     channel = client.get_channel(int(channel_id))
+    history = [message async for message in channel.history(oldest_first=True)]
+    text_hist = [f"{x.author.display_name}: {x.content}" for x in history]
+    for text in text_hist:
+        print(text)
     await channel.send(message)
 
 async def delayed_startup(channel_id, message):
